bec40b21abbfa5e2ab4619afb9b22a44bad878af
eternalSquirell <dann093@gmail.com>
2018-02-17 23:06:46 +0100

    shitload of haar cascade xml's and a few nice programs, head-mouse tracking is working

--- examples/edgeDetection.py
+++ examples/edgeDetection.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 img = cv2.imread('shapes.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
+corners = cv2.goodFeaturesToTrack(gray,25,0.1,10)
 corners = np.int0(corners)
 
 for i in corners:
