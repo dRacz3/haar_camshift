4de11acc9589b30824520ebd27ee3e5b9e9c3c33
eternalSquirell <dann093@gmail.com>
2018-02-25 15:02:46 +0100

    cleanup

--- camMouse.py
+++ camMouse.py
@@ -1,9 +1,10 @@
 import cv2
 import sys
 import pyautogui
+import numpy as np
 
 # What fille?
-cascPath = 'haar_finger.xml'
+cascPath = 'new_cascade.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
@@ -12,12 +13,27 @@ threshold = 60  #  BINARY threshold
 blurValue = 41  # GaussianBlur parameter
 bgSubThreshold = 150
 
-bgModel = cv2.createBackgroundSubtractorKNN(0, bgSubThreshold)
+bgModel = cv2.createBackgroundSubtractorKNN(4, bgSubThreshold, detectShadows=True)
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     fgmask = bgModel.apply(frame)
+
+    kernel = np.ones((3,3),np.uint8)
+    fgmask = cv2.GaussianBlur(fgmask, (1, 1), 1, 1)
+    closing = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+    gradient = cv2.morphologyEx(closing, cv2.MORPH_GRADIENT, kernel)
+
+    opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    maskStack = np.hstack((closing, gradient))
+    cv2.imshow("maskStack", maskStack)
+    openStack = np.hstack((fgmask, opening))
+    cv2.imshow("opening", openStack)
+
+
+    gray = cv2.GaussianBlur(closing, (1, 1), 1, 1)
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
 
 
@@ -31,8 +47,8 @@ while True:
         res,
         scaleFactor=1.1,
         minNeighbors=4,
-        minSize=(100, 100),
-        maxSize = (250, 400),
+        minSize=(0, 0),
+        maxSize = (500, 500),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
