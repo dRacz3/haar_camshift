5e9b142b1ae5938e5e462027c38d54289b526fa7
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-02-25 18:26:43 +0100

    Adaptive threshold added

--- src/MainProgram.py
+++ src/MainProgram.py
@@ -12,9 +12,7 @@ class program(object):
         self.logger.setLevel('DEBUG')
         self.logger.info("Program has been started!")
         self.camera = cv2.VideoCapture(0)
-
         self.operations = ImageOperations()
-
 
     def release(self):
         # When everything done, release the capture
@@ -22,25 +20,26 @@ class program(object):
         self.camera.release()
         cv2.destroyAllWindows()
 
-    #process one frame
+    # process one frame
     def ProcessOneMoreFrame(self):
         ret, frame = self.camera.read()
         result = self.process(frame)
 
-        compareResult = np.hstack((frame,result))
+        compareResult = np.hstack((frame, result))
         cv2.imshow('Result frame', compareResult)
 
     def process(self, startingImage):
-        result, mask = self.operations.removeBackground(startingImage, showIO = True)
-        result, mask = self.operations.imageThresholding(result, showIO = True)
+        result, mask = self.operations.removeBackground(startingImage, showIO=True)
+        #result, mask = self.operations.imageThresholding(result, showIO=True)
+        result, mask = self.operations.adaptiveImageThresholding(result, showIO=True)
         return result
-
 
     def run(self):
         while True:
             self.ProcessOneMoreFrame()
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
 
 app = program()
 app.run()
--- src/imageOperations.py
+++ src/imageOperations.py
@@ -8,43 +8,55 @@ class ImageOperations(object):
     def __init__(self):
         bgSubThreshold = 200
         historyCount = 100
-        self.backgroundModel = cv2.createBackgroundSubtractorKNN(historyCount, bgSubThreshold )
+        self.backgroundModel = cv2.createBackgroundSubtractorKNN(historyCount, bgSubThreshold)
 
         self.trackerWindowName = "Tracker"
         cv2.namedWindow(self.trackerWindowName)
-        cv2.createTrackbar("threshold_tolerance" ,self.trackerWindowName,150, 255, self.nothing )
+        cv2.createTrackbar("threshold_tolerance", self.trackerWindowName, 150, 255, self.nothing)
 
-    #Basic utility
+    # Basic utility
     def nothing(self, alsonothing):
         pass
 
     def showIO(self, inputImg, outputImg, name):
-        stack = np.hstack((inputImg,outputImg))
+        stack = np.hstack((inputImg, outputImg))
         cv2.imshow(name, stack)
 
-    #Returns the resulting image, and the mask
-    def removeBackground(self, image, showIO = False):
-        #Get mask
+    # Returns the resulting image, and the mask
+    def removeBackground(self, image, showIO=False):
+        # Get mask
         foregroundmask = self.backgroundModel.apply(image)
-        #Apply to original picture
+        # Apply to original picture
         result = cv2.bitwise_and(image, image, mask=foregroundmask)
         if showIO:
             self.showIO(image, result, "removeBackgroundIO")
         return result, foregroundmask
 
+    def flipImage(self, image):
+        pass
 
-    def imageThresholding(self, image, showIO = False):
+    def imageThresholding(self, image, showIO=False):
         img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        threshold = cv2.getTrackbarPos("threshold_tolerance", self.trackerWindowName)
-        ret,thresh = cv2.threshold(img,threshold,255,cv2.THRESH_BINARY)
+        threshold = cv2.getTrackbarPos(
+            "threshold_tolerance", self.trackerWindowName)
+        ret, thresh = cv2.threshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 11, 2)
         result = cv2.bitwise_and(image, image, mask=thresh)
         if showIO:
-            cv2.imshow("image thresholding result", result)
+            cv2.imshow("image thresholding result", thresh)
         return result, thresh
 
+    def adaptiveImageThresholding(self, image, showIO=False):
+        img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-    def removeNoise(self, image, showIO = False):
-        
+        thresh = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 22)
+        result = cv2.bitwise_and(image, image, mask=thresh)
+        if showIO:
+            cv2.imshow("adaptive image thresholding result", thresh)
+        return result, thresh
+
+    def removeNoise(self, image, showIO=False):
+        pass
 
     def blurFrame(self, image):
-        return result
+        pass
+#        return result
