ccee122e8784d985eb64c00252d2d4bfb37566c7
eternalSquirell <dann093@gmail.com>
2018-02-25 14:56:25 +0100

    UI fix

--- src/segmentation_example.py
+++ src/segmentation_example.py
@@ -8,11 +8,11 @@ from image_proc_utils import mouseMotionManager
 
 class CameraMouse(object):
     def __init__(self):
-        hsv_tuning = 'Tuner'
-        th_window_name = 'Threshold_tuner'
+        self.hsv_tuning = 'Tuner'
+        self.th_window_name = 'Threshold_tuner'
         self.hmin = 0
         self.smin = 171
-        self.vmin = 144
+        self.vmin = 200
         self.hmax =194
         self.smax = 255
         self.vmax = 251
@@ -22,42 +22,43 @@ class CameraMouse(object):
 
         self.tunersAreCreated = False
 
-        bgSubThreshold = 50
+        bgSubThreshold = 250
         self.backgroundModel = cv2.createBackgroundSubtractorKNN(1000, bgSubThreshold )
 
         self.cap = cv2.VideoCapture(0)
         ret, frame = self.cap.read()
         self.mouseManager = mouseMotionManager(frame)
 
+
     def nothing(self, x):
         pass
 
     def create_tuner(self):
     # create trackbars for color change
-        tunersAreCreated = True
-        cv2.namedWindow(hsv_tuning)
-        cv2.createTrackbar('H_MIN',hsv_tuning,self.hmin,255,self.nothing)
-        cv2.createTrackbar('S_MIN',hsv_tuning,self.smin,255,self.nothing)
-        cv2.createTrackbar('V_MIN',hsv_tuning,self.vmin,255,self.nothing)
-        cv2.createTrackbar('H_MAX',hsv_tuning,self.hmax,255,self.nothing)
-        cv2.createTrackbar('S_MAX',hsv_tuning,self.smax,255,self.nothing)
-        cv2.createTrackbar('V_MAX',hsv_tuning,self.vmax,255,self.nothing)
+        self.tunersAreCreated = True
+        cv2.namedWindow(self.hsv_tuning)
+        cv2.createTrackbar('H_MIN',self.hsv_tuning,self.hmin,255,self.nothing)
+        cv2.createTrackbar('S_MIN',self.hsv_tuning,self.smin,255,self.nothing)
+        cv2.createTrackbar('V_MIN',self.hsv_tuning,self.vmin,255,self.nothing)
+        cv2.createTrackbar('H_MAX',self.hsv_tuning,self.hmax,255,self.nothing)
+        cv2.createTrackbar('S_MAX',self.hsv_tuning,self.smax,255,self.nothing)
+        cv2.createTrackbar('V_MAX',self.hsv_tuning,self.vmax,255,self.nothing)
 
-        cv2.namedWindow(th_window_name)
-        cv2.createTrackbar('Th_min', th_window_name, self.th_min, 255, self.nothing)
-        cv2.createTrackbar('Th_max', th_window_name, self.th_max, 255, self.nothing)
+        cv2.namedWindow(self.th_window_name)
+        cv2.createTrackbar('Th_min', self.th_window_name, self.th_min, 255, self.nothing)
+        cv2.createTrackbar('Th_max', self.th_window_name, self.th_max, 255, self.nothing)
 
 
     def color_treshold(self,img):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         # define range of blue color in HSV
         if self.tunersAreCreated:
-            self.hmin = cv2.getTrackbarPos('H_MIN',hsv_tuning)
-            self.smin = cv2.getTrackbarPos('S_MIN',hsv_tuning)
-            self.vmin = cv2.getTrackbarPos('V_MIN',hsv_tuning)
-            self.hmax = cv2.getTrackbarPos('H_MAX',hsv_tuning)
-            self.smax = cv2.getTrackbarPos('S_MAX',hsv_tuning)
-            self.vmax = cv2.getTrackbarPos('V_MAX',hsv_tuning)
+            self.hmin = cv2.getTrackbarPos('H_MIN',self.hsv_tuning)
+            self.smin = cv2.getTrackbarPos('S_MIN',self.hsv_tuning)
+            self.vmin = cv2.getTrackbarPos('V_MIN',self.hsv_tuning)
+            self.hmax = cv2.getTrackbarPos('H_MAX',self.hsv_tuning)
+            self.smax = cv2.getTrackbarPos('S_MAX',self.hsv_tuning)
+            self.vmax = cv2.getTrackbarPos('V_MAX',self.hsv_tuning)
 
         lower = np.array([self.hmin,self.smin,self.vmin])
         upper = np.array([self.hmax,self.smax,self.vmax])
@@ -68,8 +69,8 @@ class CameraMouse(object):
 
     def gray_treshold(self, img):
         if self.tunersAreCreated:
-            self.th_min = cv2.getTrackbarPos('Th_min', th_window_name)
-            self.th_max = cv2.getTrackbarPos('Th_max', th_window_name)
+            self.th_min = cv2.getTrackbarPos('Th_min', self.th_window_name)
+            self.th_max = cv2.getTrackbarPos('Th_max', self.th_window_name)
         ret, thresh = cv2.threshold(img, self.th_min, self.th_max, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
         return thresh
 
@@ -101,7 +102,7 @@ class CameraMouse(object):
         inputimg = cv2.bitwise_not(inputimg)
 
         img, contours, hierarchy = cv2.findContours(inputimg,cv2.RETR_EXTERNAL ,cv2.CHAIN_APPROX_SIMPLE)
-
+        cv2.imshow('contours' ,img)
         drawing = np.zeros(img.shape,np.uint8)
         max_area=0
         for i in range(len(contours)):
@@ -158,12 +159,12 @@ class CameraMouse(object):
         while(True):
             # Capture frame-by-frame
             ret, frame = self.cap.read()
-            frame_no_bg = self.removeBackground(frame, showIO = False)
+            frame_no_bg = self.removeBackground(frame, showIO = True)
         #    edges = cv2.Canny(frame_no_bg,100,100)
         #    cv2.imshow('edges', edges)
 
             # Converting to HUE-SATURATION image
-            segmentedImg = self.applySegmentationBasedonHSV(frame_no_bg, showIO = False)
+            segmentedImg = self.applySegmentationBasedonHSV(frame_no_bg, showIO = True)
             normal = cv2.cvtColor(segmentedImg, cv2.COLOR_HSV2RGB)
             hsv_rgb_stack = np.hstack((segmentedImg,normal))
             #cv2.imshow("hsv-> rgb", hsv_rgb_stack)
@@ -183,7 +184,7 @@ class CameraMouse(object):
             closing = cv2.morphologyEx(erode, cv2.MORPH_CLOSE, kernel)
 
             try:
-                cx, cy, points = self.applyConvexHull(closing, originalImg = frame ,showIO = False)
+                cx, cy, points = self.applyConvexHull(closing, originalImg = frame ,showIO = True)
             except Exception as e:
                 print(e)
                 continue
@@ -200,4 +201,5 @@ class CameraMouse(object):
 ###########################x MAIN CODE x#####################################
 
 app = CameraMouse()
+app.create_tuner()
 app.runProgram()
